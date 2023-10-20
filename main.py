6e4a44dba010fb0fd0730a0776993999e4364bea
Tixerz <petete1296@gmail.com>
2023-10-20 12:43:06 -0700

    listbox and refresh func

--- main.py
+++ main.py
@@ -18,19 +18,51 @@ class contact():
     def delete(self):
         os.remove(f"/Contacts/{self.name}.con")
 
+storage = []
+showed_list = ["rayan                    92347923749237492374349387"]
 
-def add_contact_menu():
-    root = tk.Tk()
-    root.geometry("300x100")
-    root.mainloop()
+ #we r gonna save all the data here
+#for item in os.listdir("/Contacts/"):
+   # storage.append(pk.load(f"/Contacts/{item}"))
+#showed_list = ["sex    92834"] #its what we are going to show in the main listbox
+
+
 
 root =  tk.Tk()
 root.geometry("800x500")
+
+con_list = tk.Listbox(root, width=100 , height= 300)
+con_list.place(x= 10 , y= 100)
+
+def refresh():
+    global root
+    global con_list
+    global showed_list
+    con_list = tk.Listbox( root , width=100 , height= 300)
+    for item in showed_list :
+        con_list.insert('end' , item)
+    con_list.place( x= 10 , y= 100)
+refresh()
+
+def add_contact_menu():
+    
+    root = tk.Tk()
+    tk.Label(root,text="name:").place(x=10 , y =10)
+    tk.Label(root,text="number:").place(x=10 , y= 40)
+    name_entry = tk.Entry(root )
+    name_entry.place(x=100 , y=15)
+    root.geometry("300x100")
+    root.mainloop()
+    refresh()
 add_button = tk.Button(root , text= "Add" , command=add_contact_menu)
 add_button.place(x=700 , y= 350)
 
+
 entry = tk.Entry(root ,width=50)
 entry.pack(padx= 10 , pady= 30)
-search_button = tk.Button(root , text= "Search" )
+
+search_button = tk.Button(root , text= "Search" , width=5 , height=2 )
 search_button.place(x=600 , y=30)
+
+
 root.mainloop()
---
+++ search.py
@@ -0,0 +1,12 @@
+'''ur gonna create a function which takes a list
+[11:35]
+and key word
+[11:36]
+so the function is going to search for some elements and after finding it it should append it to another list which the function is going to return
+[11:37]
+and after doing it to all the elements its going to return the final list'''
+
+import re
+def search(list1 , keyword):
+    output_list =[]
+    return output_list# another list
