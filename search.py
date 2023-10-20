3ee2b5f4085ef27f52d807d2b20bc046cc4ad3dc
Tixerz <99183659+Tixerz@users.noreply.github.com>
2023-10-20 12:44:39 -0700

    Update search.py

--- search.py
+++ search.py
@@ -1,9 +1,7 @@
+#Rayane does this part :D
 '''ur gonna create a function which takes a list
-[11:35]
 and key word
-[11:36]
 so the function is going to search for some elements and after finding it it should append it to another list which the function is going to return
-[11:37]
 and after doing it to all the elements its going to return the final list'''
 
 import re
